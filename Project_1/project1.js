74aeb753711511dae6e342ec39b462fbc7c6ee5c
NTB072\danvu <danvuk11@gmail.com>
2023-10-16 23:50:50 -0400

    Add more blending functions

--- Project_1/project1.js
+++ Project_1/project1.js
@@ -22,15 +22,58 @@ function composite( bgImg, fgImg, fgOpac, fgPos )
                 fgColor = getColor(fgData, fgColorIndices);
             }
 
+            //Alpha blending
+            // if(fgColor[3] != 0 && bgColor[3] != 0)
+            // {
+            //     const fgAlpha = fgColor[3] * fgOpac;
+            //     const newAlpha = fgAlpha  + bgColor[3] * (1 - fgAlpha);
+            //     bgColor[0] = (fgColor[0] * fgAlpha + bgColor[0] * bgColor[3] * (1 - fgAlpha)) / newAlpha;
+            //     bgColor[1] = (fgColor[1] * fgAlpha + bgColor[1] * bgColor[3] * (1 - fgAlpha)) / newAlpha;
+            //     bgColor[2] = (fgColor[2] * fgAlpha + bgColor[2] * bgColor[3] * (1 - fgAlpha)) / newAlpha;
+            //     bgColor[3] = newAlpha;
+            // }
+
+            //Additive blending
+            // if(fgColor[3] != 0 && bgColor[3] != 0)
+            // {
+            //     const fgAlpha = fgColor[3] * fgOpac;
+            //     const newAlpha = saturate(fgAlpha + bgColor[3]);
+            //     bgColor[0] = (fgColor[0] * fgAlpha + bgColor[0] * bgColor[3]) / newAlpha;
+            //     bgColor[1] = (fgColor[1] * fgAlpha + bgColor[1] * bgColor[3]) / newAlpha;
+            //     bgColor[2] = (fgColor[2] * fgAlpha + bgColor[2] * bgColor[3]) / newAlpha;
+            //     bgColor[3] = newAlpha;
+            // }
+
+            //Difference blending
+            // if(fgColor[3] != 0 && bgColor[3] != 0)
+            // {
+            //     const fgAlpha = fgColor[3] * fgOpac;
+            //     const newAlpha = saturate(abs(fgAlpha + bgColor[3]));
+            //     bgColor[0] = abs(fgColor[0] * fgAlpha - bgColor[0] * bgColor[3]) / newAlpha;
+            //     bgColor[1] = abs(fgColor[1] * fgAlpha - bgColor[1] * bgColor[3]) / newAlpha;
+            //     bgColor[2] = abs(fgColor[2] * fgAlpha - bgColor[2] * bgColor[3]) / newAlpha;
+            //     bgColor[3] = newAlpha;
+            // }
+
+            //Myltiply blending
+            // if(fgColor[3] != 0 && bgColor[3] != 0)
+            // {
+            //     const fgAlpha = fgColor[3] * fgOpac;
+            //     bgColor[0] = fgColor[0] * bgColor[0] * fgAlpha + (1 - fgAlpha) * bgColor[0];
+            //     bgColor[1] = fgColor[1] * bgColor[1] * fgAlpha + (1 - fgAlpha) * bgColor[1];
+            //     bgColor[2] = fgColor[2] * bgColor[2] * fgAlpha + (1 - fgAlpha) * bgColor[2];
+            //     bgColor[3] = fgAlpha;
+            // }
+
+            //Screen blending
             if(fgColor[3] != 0 && bgColor[3] != 0)
             {
-                const fgAlpha = fgColor[3] * fgOpac;
-                const newAlpha = fgAlpha  + bgColor[3] * (1 - fgAlpha);
-                bgColor[0] = (fgColor[0] * fgAlpha + bgColor[0] * bgColor[3] * (1 - fgAlpha)) / newAlpha;
-                bgColor[1] = (fgColor[1] * fgAlpha + bgColor[1] * bgColor[3] * (1 - fgAlpha)) / newAlpha;
-                bgColor[2] = (fgColor[2] * fgAlpha + bgColor[2] * bgColor[3] * (1 - fgAlpha)) / newAlpha;
-                bgColor[3] = newAlpha;
+                bgColor[0] = 1 - (1 - fgColor[0]) * (1 - bgColor[0]);
+                bgColor[1] = 1 - (1 - fgColor[1]) * (1 - bgColor[1]);
+                bgColor[2] = 1 - (1 - fgColor[2]) * (1 - bgColor[2]);
+                bgColor[3] = bgColor[3];
             }
+
             writeColor(bgColor, bgData, bgColorIndices);
         }
     }
@@ -65,4 +108,12 @@ function setPixel(imageData, x, y, r, g, b, a) {
     imageData.data[index * 4 + 1] = g;
     imageData.data[index * 4 + 2] = b;
     imageData.data[index * 4 + 3] = a;
-  }
+}
+
+function saturate(val) {
+    return val > 1.0 ? 1.0 : (val < 0.0 ? 0 : val);
+}
+
+function abs(val) {
+    return val >= 0.0 ? val : -val;
+}
